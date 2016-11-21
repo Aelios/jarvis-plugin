3375eaddc6d1e1cad8999be3566c4880f70b8993
Aelios <lefevre.nicolas88@gmail.com>
2016-11-21 12:24:33 +0000

    init

--- fr/functions.sh
+++ fr/functions.sh
@@ -6,3 +6,13 @@
 # jv for JarVis
 # pg for PluGin
 # XX can be a two letters code for your plugin, ex: ww for Weather Wunderground
+jv_pg_wz_check_traffic() {
+local json=$(curl -s "https://www.waze.com/row-RoutingManager/routingRequest?from=x%3A$fromX+y%3A$fromY&to=x%3A$toX+y%3A$toY&at=0&returnJSON=true&returnGeometries=false&returnInstructions=true&timeout=60000&nPaths=3&clientVersion=4.0.0&options=AVOID_TRAILS%3At%2CALLOW_UTURNS%3At")
+local crossTime=$(echo "$json" | jq -r -s 'map(.alternatives[0].response.results[].crossTime) | add')
+local crossTimeWithoutRealTime=$(echo "$json" | jq -r -s 'map(.alternatives[0].response.results[].crossTimeWithoutRealTime) | add')
+local start="Vous allez être retardé de "
+local difference=$(($crossTime-$crossTimeWithoutRealTime))
+local end=" secondes"
+echo $start$difference$end
+}
+
